5a577567f7aff83a1bd9a1df403cc6644e1abf49
Lucas Gauvain <lucasgauvain@gmail.com>
2023-03-27 21:56:30 +0200

    added socket chat

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,11 +6,32 @@ const App = () => {
     socket.emit('message', { message: 'Hello' });
   }
   
-  
+  const user1JoinRoom1 = () => {
+    socket.emit('join room', { roomId: 'room1', username: 'name1' });
+  }
+
+
+  const user2joinRoom1 = () => {
+    socket.emit('join room', { roomId: 'room1', username: 'name2' });
+  }
+
+  const getRooms = () => {
+    socket.emit('get rooms');
+  }
+
+  const getRoomUsers = () => {
+    socket.emit('get room users', { roomId: 'room1' });
+  }
+
+
   return (
     <div className="App">
       <p>Oh ma gueule</p>
       <button onClick={sendMessage}>send message</button>
+      <button onClick={user1JoinRoom1}>join room</button>
+      <button onClick={user2joinRoom1}>join room</button>
+      <button onClick={getRooms}>get rooms</button>
+      <button onClick={getRoomUsers}>get room users</button>
     </div>
   );
 };
